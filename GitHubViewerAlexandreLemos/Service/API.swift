8ab9d7b8a297c8c79d262536a833caf02a9e228c
alslemos <alexandre-ls@hotmail.com>
2025-02-05 13:19:24 -0300

    created the entity and api files

---
+++ GitHubViewerAlexandreLemos/Service/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  GitHubViewerAlexandreLemos
+//
+//  Created by Alexandre Lemos da Silva on 05/02/25.
+//
+
+import Foundation
+
---
+++ GitHubViewerAlexandreLemos/Service/Entities.swift
@@ -0,0 +1,8 @@
+//
+//  Entities.swift
+//  GitHubViewerAlexandreLemos
+//
+//  Created by Alexandre Lemos da Silva on 05/02/25.
+//
+
+import Foundation
