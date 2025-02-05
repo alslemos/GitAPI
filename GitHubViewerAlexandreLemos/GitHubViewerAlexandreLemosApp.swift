3d686b09ff1af5a58fa1a8c06c64949dc6dba040
alslemos <alexandre-ls@hotmail.com>
2025-02-05 11:28:47 -0300

    Initial Commit

---
+++ GitHubViewerAlexandreLemos/GitHubViewerAlexandreLemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubViewerAlexandreLemosApp.swift
+//  GitHubViewerAlexandreLemos
+//
+//  Created by Alexandre Lemos da Silva on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubViewerAlexandreLemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
