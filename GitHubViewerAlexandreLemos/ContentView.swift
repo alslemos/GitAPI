291720a5a2a74a986242d8f970b3f94aa6331706
alslemos <alexandre-ls@hotmail.com>
2025-02-05 11:36:01 -0300

    Initial commit creating the project plus starting the UI

--- GitHubViewerAlexandreLemos/ContentView.swift
+++ GitHubViewerAlexandreLemos/ContentView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // main variable to call API
+    @State private var gitUserToSearch: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            
+            TextField(
+                "Username",
+                text: $gitUserToSearch, prompt: Text("Username")
+            )
+            .disableAutocorrection(true)
+            
             Text("Hello, world!")
         }
         .padding()
